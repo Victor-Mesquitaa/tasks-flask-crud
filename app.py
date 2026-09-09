9da035e62908f40a3be1202e5badb484a952cc56
Luiz Victor Mesquita <luizvictor2513@gmail.com>
2026-09-09 09:30:43 -0300

    Iniciando o desenvolvimenta da API - Create

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.tasks import Task
 
-#__name__ = __main__
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
+# CRUD
+# Create, Read, Update and Delete
+# Tabela: Tarefa
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description"))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "New task created sucessfuly",})
 
 if __name__ == "__main__":
     app.run(debug=True)
